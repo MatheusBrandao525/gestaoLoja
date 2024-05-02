3909104da843f3cb5389b4beaa4edbaf0225d8b0
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-01 23:54:54 -0300

    feat: :memo: Corrigindo exibição de cada tela, e alterando rota para cadastro do produto.

--- components/menuLateral.php
+++ components/menuLateral.php
@@ -142,7 +142,7 @@
                         </div>
                     </a>
                     <div class="dropdown-content">
-                        <a href="#" onclick="loadContent('cadastroProdutosForm')">Cadastrar Produto</a>
+                        <a href="cadastroProduto">Cadastrar Produto</a>
                         <a href="#" onclick="loadContent('ver-produtos')">Ver Produtos</a>
                     </div>
                 </li>
@@ -218,220 +218,13 @@
             </div>
         </div>
         <!-- ========================= Main ==================== -->
-        <div class="main">
 
-
-            <!-- ======================= Cards ================== -->
-            <div class="cardBox">
-                <div class="card">
-                    <div>
-                        <div class="numbers">154</div>
-                        <div class="cardName">Produtos Cadastrados</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <i class="fas fa-box-open"></i>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">80</div>
-                        <div class="cardName">Clientes Cadastrados</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <i class="fas fa-users"></i>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">24</div>
-                        <div class="cardName">Categorias Cadastradas</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <i class="fas fa-tags"></i>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">2</div>
-                        <div class="cardName">Usuarios Cadastrados</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <i class="fas fa-id-card"></i>
-                    </div>
-                </div>
-            </div>
-
-            <!-- ================ Order Details List ================= -->
-            <div class="details">
-                <div class="recentOrders">
-                    <div class="cardHeader">
-                        <h2>Produtos Cadastrados Recentemente</h2>
-                        <a href="#" class="btn">Ver Todos</a>
-                    </div>
-
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Nome</td>
-                                <td>Preço</td>
-                                <td>Categoria</td>
-                                <td>Detalhes</td>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr>
-                                <td>Produto de teste 01</td>
-                                <td>R$ 1.200,00</td>
-                                <td>teste01</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 02</td>
-                                <td>R$ 110,00</td>
-                                <td>teste02</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 03</td>
-                                <td>R$ 1.200,00</td>
-                                <td>teste01</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 04</td>
-                                <td>R$ 620,00</td>
-                                <td>teste02</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 05</td>
-                                <td>R$ 1.200,00</td>
-                                <td>teste01</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 06</td>
-                                <td>R$ 110,00</td>
-                                <td>teste02</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 07</td>
-                                <td>R$ 1.200,00</td>
-                                <td>teste01</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-
-                            <tr>
-                                <td>Produto de teste 08</td>
-                                <td>R$ 620,00</td>
-                                <td>teste02</td>
-                                <td><button class="details-btn">Detalhes</button></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-                <!-- ================= New Customers ================ -->
-                <div class="recentCustomers">
-                    <div class="cardHeader">
-                        <h2>Clientes Cadastrados Recentemente</h2>
-                    </div>
-
-                    <table>
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>David <br> <span>Italy</span></h4>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td width="60px">
-                                <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
-                            </td>
-                            <td>
-                                <h4>Amit <br> <span>India</span></h4>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- =========== Scripts =========  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="public/assets/js/menuLateral.js"></script>
+    <script src="public/assets/js/cadastroProdutoForm.js"></script>
 
     <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
@@ -487,6 +280,7 @@
                 .catch(error => console.error('Error loading the page: ', error));
         }
     </script>
+
 </body>
 
 </html>

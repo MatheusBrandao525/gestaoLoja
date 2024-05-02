c97f1079f0cdbc84e4e342bbacbc9247eea6021e
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-02 15:59:19 -0400

    feat: :memo: View home para quando o usuario entrar na aplicação.

--- views/home.php
+++ views/home.php
@@ -1,3 +1,214 @@
 <?php
 require 'components/menuLateral.php';
 ?>
+<div class="main">
+
+
+    <!-- ======================= Cards ================== -->
+    <div class="cardBox">
+        <div class="card">
+            <div>
+                <div class="numbers">154</div>
+                <div class="cardName">Produtos Cadastrados</div>
+            </div>
+
+            <div class="iconBx">
+                <i class="fas fa-box-open"></i>
+            </div>
+        </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">80</div>
+                <div class="cardName">Clientes Cadastrados</div>
+            </div>
+
+            <div class="iconBx">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">24</div>
+                <div class="cardName">Categorias Cadastradas</div>
+            </div>
+
+            <div class="iconBx">
+                <i class="fas fa-tags"></i>
+            </div>
+        </div>
+
+        <div class="card">
+            <div>
+                <div class="numbers">2</div>
+                <div class="cardName">Usuarios Cadastrados</div>
+            </div>
+
+            <div class="iconBx">
+                <i class="fas fa-id-card"></i>
+            </div>
+        </div>
+    </div>
+
+    <!-- ================ Order Details List ================= -->
+    <div class="details">
+        <div class="recentOrders">
+            <div class="cardHeader">
+                <h2>Produtos Cadastrados Recentemente</h2>
+                <a href="#" class="btn">Ver Todos</a>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Preço</td>
+                        <td>Categoria</td>
+                        <td>Detalhes</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Star Refrigerator</td>
+                        <td>R$ 1200</td>
+                        <td>Paid</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Dell Laptop</td>
+                        <td>R$ 110</td>
+                        <td>Due</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Apple Watch</td>
+                        <td>R$ 1200</td>
+                        <td>Paid</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Addidas Shoes</td>
+                        <td>R$ 620</td>
+                        <td>Due</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Star Refrigerator</td>
+                        <td>R$ 1200</td>
+                        <td>Paid</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Dell Laptop</td>
+                        <td>R$ 110</td>
+                        <td>Due</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Apple Watch</td>
+                        <td>R$ 1200</td>
+                        <td>Paid</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+
+                    <tr>
+                        <td>Addidas Shoes</td>
+                        <td>R$ 620</td>
+                        <td>Due</td>
+                        <td><button class="details-btn">Detalhes</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- ================= New Customers ================ -->
+        <div class="recentCustomers">
+            <div class="cardHeader">
+                <h2>Clientes Cadastrados Recentemente</h2>
+            </div>
+
+            <table>
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>David <br> <span>Italy</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>Amit <br> <span>India</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>David <br> <span>Italy</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>Amit <br> <span>India</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>David <br> <span>Italy</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>Amit <br> <span>India</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>David <br> <span>Italy</span></h4>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="60px">
+                        <div class="imgBx"><img src="public/assets/img/placeholder.jpg" alt=""></div>
+                    </td>
+                    <td>
+                        <h4>Amit <br> <span>India</span></h4>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+</div>

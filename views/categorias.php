f889faa676975011c0314fa40363be8857e9f568
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-02 22:45:40 -0300

    fix: :bug: Corrigido erra de não exibir a imagem da categoria - nome da variavel estava errado.

--- views/categorias.php
+++ views/categorias.php
@@ -7,19 +7,19 @@ require_once 'components/menuLateral.php';
     <div class="centralizar">
         <div class="categorias-container">
             <?php foreach ($categoriasData as $categoria) { ?>
-            <div class="categoria-item">
-                <div class="categoria-imagem">
-                    <img src="public/assets/img/categorias/<?php echo $categoriasData['imagem_categria'];?>" alt="Imagem da Categoria">
-                </div>
-                <div class="categoria-info">
-                    <span class="categoria-nome"><?php echo htmlspecialchars($categoria['nome_categoria']); ?></span>
-                    <div class="categoria-opcoes">
-                        <button class="btn-editar">Editar</button>
-                        <button class="btn-excluir">Excluir</button>
-                        <button class="btn-ver-produtos">Ver Produtos</button>
+                <div class="categoria-item">
+                    <div class="categoria-imagem">
+                        <img src="public/assets/img/categorias/<?php echo $categoria['imagem_categria']; ?>" alt="Imagem da Categoria">
+                    </div>
+                    <div class="categoria-info">
+                        <span class="categoria-nome"><?php echo htmlspecialchars($categoria['nome_categoria']); ?></span>
+                        <div class="categoria-opcoes">
+                            <button class="btn-editar">Editar</button>
+                            <button class="btn-excluir">Excluir</button>
+                            <button class="btn-ver-produtos">Ver Produtos</button>
+                        </div>
                     </div>
                 </div>
-            </div>
             <?php } ?>
         </div>
     </div>

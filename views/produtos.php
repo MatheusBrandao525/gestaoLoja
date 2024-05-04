baaf8bcc13b768919a9ee4ea51078e6623bf3a7d
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-03 23:53:36 -0300

    feat: :sparkles: Botao excluir produto que ao ser clicado chama a função jquery de envio de requisição.

--- views/produtos.php
+++ views/produtos.php
@@ -15,7 +15,7 @@ require_once 'components/menuLateral.php';
                         <span class="categoria-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
                         <div class="categoria-opcoes">
                             <button class="btn-editar">Editar</button>
-                            <button class="btn-excluir" data-produto-id="<?php echo $produto['produto_id'] ?>" data-imagem-produto="<?php echo $produto['imagem1']; ?>">Excluir</button>
+                            <button class="btn-excluir-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Excluir</button>
                             <button class="btn-ver-produtos">Ver Produtos</button>
                         </div>
                     </div>

15673d4c8efa1379d78f26ada125740ea9b25c14
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-05 20:40:58 -0300

    feat: :sparkles: Alterando nome das classes para serem exclusivos desta tela, redirecionando usuario para tela altera produto.

--- views/produtos.php
+++ views/produtos.php
@@ -5,18 +5,20 @@ require_once 'components/menuLateral.php';
 ?>
 <div class="main">
     <div class="centralizar">
-        <div class="categorias-container">
+        <div class="produtos-container">
             <?php foreach ($produtosData as $produto) { ?>
-                <div class="categoria-item">
-                    <div class="categoria-imagem">
-                        <img src="public/assets/img/produtos/<?php echo $produto['imagem1']; ?>" alt="Imagem da Categoria">
+                <div class="produto-item">
+                    <div class="produto-imagem">
+                        <img src="public/assets/img/produtos/<?php echo $produto['imagem1']; ?>" alt="Imagem do produto">
                     </div>
-                    <div class="categoria-info">
-                        <span class="categoria-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
-                        <div class="categoria-opcoes">
-                            <button class="btn-editar">Editar</button>
+                    <div class="produto-info">
+                        <span class="produto-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
+                        <div class="produto-opcoes">
+                            <form action="editarProduto" method="post">
+                                <input type="hidden" value="<?php echo $produto['produto_id']; ?>">
+                                <button type="submit" class="btn-editar-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Editar</button>
+                            </form>
                             <button class="btn-excluir-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Excluir</button>
-                            <button class="btn-ver-produtos">Ver Produtos</button>
                         </div>
                     </div>
                 </div>

f67edbe09ced81093c2006cb410809860dec0ed4
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-05 22:12:47 -0300

    fix: :bug: Input sem atributo name.

--- views/produtos.php
+++ views/produtos.php
@@ -15,7 +15,7 @@ require_once 'components/menuLateral.php';
                         <span class="produto-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
                         <div class="produto-opcoes">
                             <form action="editarProduto" method="post">
-                                <input type="hidden" value="<?php echo $produto['produto_id']; ?>">
+                                <input type="hidden" name="produtoId" value="<?php echo $produto['produto_id']; ?>">
                                 <button type="submit" class="btn-editar-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Editar</button>
                             </form>
                             <button class="btn-excluir-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Excluir</button>

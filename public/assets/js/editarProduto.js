7d3c8deacb817b9ca4f3d904288640638ffdd3b2
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-06 13:10:52 -0400

    feat: :sparkles: Função para enviar a requisição de exclusão de imagem do produto sem ataulizar a página.

--- public/assets/js/editarProduto.js
+++ public/assets/js/editarProduto.js
@@ -15,11 +15,38 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
           console.log("Sucesso:", data);
           alert("Produto alterado com sucesso!");
+          window.location.reload();
         })
         .catch((error) => {
           console.error("Erro:", error);
           alert("Erro ao alterar produto. Tente novamente.");
         });
     }
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var deleteIcons = document.querySelectorAll('.icone-apagar-imagem-produto');
+  deleteIcons.forEach(function(icon) {
+      icon.addEventListener('click', function() {
+          var imagemId = this.getAttribute('data-imagem-id');
+          var produtoId = this.getAttribute('data-produto-id');
+          if (confirm('Você realmente deseja excluir esta imagem?')) {
+              var xhr = new XMLHttpRequest();
+              xhr.open('POST', 'excluirImagemProduto', true);
+              xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+              xhr.onload = function() {
+                  if (xhr.status === 200) {
+                      alert('Imagem excluída com sucesso!');
+                      console.log(this.response);
+                      window.location.reload();
+                  } else {
+                      alert('Erro ao excluir imagem.');
+                  }
+              };
+              xhr.send('produtoId=' + produtoId + '&imagem=' + imagemId);
+          }
+      });
   });
 });

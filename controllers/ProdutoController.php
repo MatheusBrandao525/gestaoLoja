e1ac48037a4a3ac58ca836fd6071a14594fbafaf
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-06 00:38:46 -0300

    feat: :sparkles: Adicionano fução que sera responsavel por alterar dados dos produtos.

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -145,4 +145,10 @@ class ProdutoController
             }
         }
     }
+
+    public function alterarDadosProduto()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
 }

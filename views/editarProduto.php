636c5d67a66ac34c41089f2732d3b609fb472769
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-06 13:10:08 -0400

    feat: :sparkles: Preenchedo campos do formulario de alteração de produtos com dados do database.

--- views/editarProduto.php
+++ views/editarProduto.php
@@ -7,18 +7,19 @@ $produtoData = $produtoController->mostraDadosProduto();
 <div class="main">
     <div class="centralizar">
         <form id="formularioEditarProduto" action="alteraProduto" method="post">
+            <input type="hidden" name="produtoId" value="<?php echo $produtoData['produto_id'];?>">
             <div class="row">
                 <div class="column">
                     <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($produtoData['nome']); ?>" required>
+                    <input type="text" id="nome" name="altera-nome" value="<?php echo htmlspecialchars($produtoData['nome']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="codigo">Código:</label>
-                    <input type="text" id="codigo" name="codigo" value="<?php echo htmlspecialchars($produtoData['codigo']); ?>" required>
+                    <input type="text" id="codigo" name="altera-codigo" value="<?php echo htmlspecialchars($produtoData['codigo']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="exibirPreco">Deseja exibir o preço?</label>
-                    <select id="exibirPreco" name="exibirPreco" required>
+                    <select id="exibirPreco" name="altera-exibirPreco" required>
                         <option value="<?php echo htmlspecialchars($produtoData['exibe_preco']); ?>"><?php echo htmlspecialchars($produtoData['status_exibe_preco']); ?></option>
                         <option value="1">Sim</option>
                         <option value="0">Não</option>
@@ -26,25 +27,25 @@ $produtoData = $produtoController->mostraDadosProduto();
                 </div>
                 <div class="column">
                     <label for="precoCusto">Preço de Custo:</label>
-                    <input type="number" id="precoCusto" name="precoCusto" value="<?php echo htmlspecialchars($produtoData['preco_custo']); ?>" required>
+                    <input type="number" id="precoCusto" name="altera-precoCusto" value="<?php echo htmlspecialchars($produtoData['preco_custo']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="precoUnitario">Preço Unitário:</label>
-                    <input type="number" id="precoUnitario" name="precoUnitario" value="<?php echo htmlspecialchars($produtoData['preco_unitario']); ?>" required>
+                    <input type="number" id="precoUnitario" name="altera-precoUnitario" value="<?php echo htmlspecialchars($produtoData['preco_unitario']); ?>" required>
                 </div>
             </div>
             <div class="row">
                 <div class="column">
                     <label for="modelos">Modelos:</label>
-                    <input type="text" id="modelos" name="modelos" value="<?php echo htmlspecialchars($produtoData['modelos']); ?>" required>
+                    <input type="text" id="modelos" name="altera-modelos" value="<?php echo htmlspecialchars($produtoData['modelos']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="cor">Cor:</label>
-                    <input type="text" id="cor" name="cor" value="<?php echo htmlspecialchars($produtoData['cor']); ?>" required>
+                    <input type="text" id="cor" name="altera-cor" value="<?php echo htmlspecialchars($produtoData['cor']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="destaque">É Destaque?</label>
-                    <select id="destaque" name="destaque" value="<?php echo htmlspecialchars($produtoData['destaque']); ?>" required>
+                    <select id="destaque" name="altera-destaque" value="<?php echo htmlspecialchars($produtoData['destaque']); ?>" required>
                         <option value="<?php echo htmlspecialchars($produtoData['destaque']); ?>"><?php echo htmlspecialchars($produtoData['status_destaque']); ?></option>
                         <option value="1">Sim</option>
                         <option value="0">Não</option>
@@ -52,11 +53,11 @@ $produtoData = $produtoController->mostraDadosProduto();
                 </div>
                 <div class="column">
                     <label for="tamanhos">Tamanhos:</label>
-                    <input type="text" id="tamanhos" name="tamanhos" value="<?php echo htmlspecialchars($produtoData['tamanhos']); ?>" required>
+                    <input type="text" id="tamanhos" name="altera-tamanhos" value="<?php echo htmlspecialchars($produtoData['tamanhos']); ?>" required>
                 </div>
                 <div class="column">
                     <label for="categoriaid">Categoria</label>
-                    <select id="categoriaid" name="categoriaid" required>
+                    <select id="categoriaid" name="altera-categoriaid" required>
                         <option value="<?php echo htmlspecialchars($produtoData['categoria_id']); ?>"><?php echo htmlspecialchars($produtoData['nome_categoria']); ?></option>
                         <option value="1">teste1</option>
                         <option value="2">teste2</option>
@@ -65,19 +66,22 @@ $produtoData = $produtoController->mostraDadosProduto();
             </div>
             <div class="full-width">
                 <label for="descricao">Descrição:</label>
-                <textarea id="descricao" name="descricao" rows="4" required><?php echo htmlspecialchars($produtoData['descricao']); ?></textarea>
+                <textarea id="descricao" name="altera-descricao" rows="4" required><?php echo htmlspecialchars($produtoData['descricao']); ?></textarea>
             </div>
             <div class="row">
                 <div class="column">
                     <div class="preview-imagem-atual-produto">
-                        <span>Imagem 01 Atual</span>
+                            <span>Imagem 01 Atual</span>
                         <img src="public/assets/img/produtos/<?php echo $produtoData['imagem1']; ?>" alt="">
                     </div>
                 </div>
-                <?php if ($produtoData['imagem3'] != null && $produtoData['imagem3'] != '') { ?>
+                <?php if ($produtoData['imagem2'] != null && $produtoData['imagem2'] != '') { ?>
                     <div class="column">
                         <div class="preview-imagem-atual-produto">
+                        <div class="container-imagem-atual-produto">
                             <span>Imagem 02 Atual</span>
+                            <div class="icone-apagar-imagem-produto" data-produto-id="<?php echo $produtoData['produto_id'];?>" data-imagem-id="imagem2"><i class="fa fa-trash" aria-hidden="true"></i></div>
+                        </div>
                             <img src="public/assets/img/produtos/<?php echo $produtoData['imagem2']; ?>" alt="">
                         </div>
                     </div>
@@ -85,7 +89,10 @@ $produtoData = $produtoController->mostraDadosProduto();
                 <?php if ($produtoData['imagem3'] != null && $produtoData['imagem3'] != '') { ?>
                     <div class="column">
                         <div class="preview-imagem-atual-produto">
+                        <div class="container-imagem-atual-produto">
                             <span>Imagem 03 Atual</span>
+                            <div class="icone-apagar-imagem-produto" data-produto-id="<?php echo $produtoData['produto_id'];?>" data-imagem-id="imagem3"><i class="fa fa-trash" aria-hidden="true"></i></div>
+                        </div>
                             <img src="public/assets/img/produtos/<?php echo $produtoData['imagem3']; ?>" alt="">
                         </div>
                     </div>
@@ -95,19 +102,19 @@ $produtoData = $produtoController->mostraDadosProduto();
                 <div class="column">
                     <div class="">
                         <label for="imagem1">Imagem:</label>
-                        <input type="file" id="imagem1" name="imagem1" required>
+                        <input type="file" id="imagem1" name="altera-imagem1">
                     </div>
                 </div>
                 <div class="column">
                     <div class="">
                         <label for="imagem2">Imagem:</label>
-                        <input type="file" id="imagem2" name="imagem2">
+                        <input type="file" id="imagem2" name="altera-imagem2">
                     </div>
                 </div>
                 <div class="column">
                     <div class="">
                         <label for="imagem3">Imagem:</label>
-                        <input type="file" id="imagem3" name="imagem3">
+                        <input type="file" id="imagem3" name="altera-imagem3">
                     </div>
                 </div>
             </div>

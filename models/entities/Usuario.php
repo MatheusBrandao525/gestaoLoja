39a4515d4d40ce6b1fb23dbd1c923c1273d4dff9
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-06 20:52:03 -0400

    feat: :sparkles: Criando a classe de usuario.

---
+++ models/entities/Usuario.php
@@ -0,0 +1,93 @@
+<?php
+
+class Usuario
+{
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+    private $status;
+    private $foto;
+
+
+    public function __construct($nome, $email, $senha, $status, $foto)
+    {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->senha = $senha;
+        $this->status = $status;
+        $this->foto = $foto;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha): self
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function setFoto($foto): self
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+}

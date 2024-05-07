7cfb74c8cd6fa288ebfc5864aff47efe133f4eff
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-07 17:56:40 -0400

    feat: :sparkles: Enviar requisição para exclusão de usuário sem atualizar a página.

---
+++ public/assets/js/excluirUsuario.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botoesExcluir = document.querySelectorAll(".btn-excluir-usuario");
+  
+    botoesExcluir.forEach(function (botao) {
+      botao.addEventListener("click", function () {
+        const usuarioId = this.getAttribute("data-usuario-id");
+        if (confirm("Tem certeza que deseja excluir este usuário?")) {
+          fetch("excluirUsuario", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              usuarioId: usuarioId,
+            }),
+          })
+            .then((response) => response.json()) // Agora estamos pegando a resposta como JSON diretamente
+            .then((data) => {
+              if (data.success) {
+                alert("Usuário excluído com sucesso!");
+                window.location.reload(); // Recarrega a página para atualizar a lista
+              } else {
+                alert("Erro ao excluir o usuário: " + data.error);
+              }
+            })
+            .catch((error) => {
+              console.error("Erro:", error);
+              alert("Erro ao excluir usuário.");
+            });
+        }
+      });
+    });
+  });
+  

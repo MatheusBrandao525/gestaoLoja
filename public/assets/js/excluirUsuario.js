33aade4ba3cfc97377c357edb7a4fa670ab9636f
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-07 23:47:48 -0300

    feat: :sparkles: Códio javascript para enviar dados do usuário a ser excluido sem atualizar a página.

--- public/assets/js/excluirUsuario.js
+++ public/assets/js/excluirUsuario.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botoesExcluir = document.querySelectorAll(".btn-excluir-usuario");
-  
-    botoesExcluir.forEach(function (botao) {
-      botao.addEventListener("click", function () {
-        const usuarioId = this.getAttribute("data-usuario-id");
-        if (confirm("Tem certeza que deseja excluir este usuário?")) {
-          fetch("excluirUsuario", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              usuarioId: usuarioId,
-            }),
+  const botoesExcluir = document.querySelectorAll(".btn-excluir-usuario");
+
+  botoesExcluir.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+      const usuarioId = this.getAttribute("data-usuario-id");
+      const fotoUsuario = this.getAttribute("data-foto-usuario");
+      if (confirm("Tem certeza que deseja excluir este usuário?")) {
+        fetch("excluirUsuario", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            usuarioId: usuarioId,
+            fotoUsuario: fotoUsuario,
+          }),
+        })
+          .then((response) => response.json()) // Agora estamos pegando a resposta como JSON diretamente
+          .then((data) => {
+            if (data.success) {
+              alert("Usuário excluído com sucesso!");
+              window.location.reload(); // Recarrega a página para atualizar a lista
+            } else {
+              alert("Erro ao excluir o usuário: " + data.error);
+            }
           })
-            .then((response) => response.json()) // Agora estamos pegando a resposta como JSON diretamente
-            .then((data) => {
-              if (data.success) {
-                alert("Usuário excluído com sucesso!");
-                window.location.reload(); // Recarrega a página para atualizar a lista
-              } else {
-                alert("Erro ao excluir o usuário: " + data.error);
-              }
-            })
-            .catch((error) => {
-              console.error("Erro:", error);
-              alert("Erro ao excluir usuário.");
-            });
-        }
-      });
+          .catch((error) => {
+            console.error("Erro:", error);
+            alert("Erro ao excluir usuário.");
+          });
+      }
     });
   });
-  
+});

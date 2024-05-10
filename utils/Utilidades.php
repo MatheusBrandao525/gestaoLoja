689d9f5ec20cab8a025bf86f2d141a7a2372a509
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-10 17:29:30 -0400

    feat: :sparkles: Função para converter datas para formato compativel com o banco de dados mysql.

--- utils/Utilidades.php
+++ utils/Utilidades.php
@@ -9,4 +9,13 @@ class Utilidades
             throw new Exception("O campo {$nomeCampo} deve ser preenchido!");
         }
     }
+
+    function formatarDataParaMySQL($dateStr) {
+        if ($dateStr === null) {
+            return null;
+        }
+        $date = new DateTime($dateStr);
+        return $date->format('Y-m-d H:i:s');
+    }
+    
 }

84de38ce692a901c2f358b0145ce7a1a9fcb3010
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-13 10:14:26 -0400

    feat: :sparkles: Incluindo campo para busca de produtos.

--- views/produtos.php
+++ views/produtos.php
@@ -4,24 +4,29 @@ $produtosData = $produtos->exibirTodosOsProdutos();
 require_once 'components/menuLateral.php';
 ?>
 <div class="main">
+    <div class="busca-produtos">
+        <input type="text" id="busca" placeholder="Digite o nome ou código do produto..." onkeyup="pesquisaProdutos()">
+    </div>
     <div class="centralizar">
         <div class="produtos-container">
             <?php foreach ($produtosData as $produto) { ?>
-                <div class="produto-item">
-                    <div class="produto-imagem">
-                        <img src="public/assets/img/produtos/<?php echo $produto['imagem1']; ?>" alt="Imagem do produto">
-                    </div>
-                    <div class="produto-info">
-                        <span class="produto-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
-                        <div class="produto-opcoes">
-                            <form action="editarProduto" method="post">
-                                <input type="hidden" name="produtoId" value="<?php echo $produto['produto_id']; ?>">
-                                <button type="submit" class="btn-editar-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Editar</button>
-                            </form>
-                            <button class="btn-excluir-produto" data-produto-id="<?php echo $produto['produto_id']; ?>">Excluir</button>
-                        </div>
+            <div class="produto-item">
+                <div class="produto-imagem">
+                    <img src="public/assets/img/produtos/<?php echo $produto['imagem1']; ?>" alt="Imagem do produto">
+                </div>
+                <div class="produto-info">
+                    <span class="produto-nome"><?php echo htmlspecialchars($produto['nome']); ?></span>
+                    <div class="produto-opcoes">
+                        <form action="editarProduto" method="post">
+                            <input type="hidden" name="produtoId" value="<?php echo $produto['produto_id']; ?>">
+                            <button type="submit" class="btn-editar-produto"
+                                data-produto-id="<?php echo $produto['produto_id']; ?>">Editar</button>
+                        </form>
+                        <button class="btn-excluir-produto"
+                            data-produto-id="<?php echo $produto['produto_id']; ?>">Excluir</button>
                     </div>
                 </div>
+            </div>
             <?php } ?>
         </div>
     </div>

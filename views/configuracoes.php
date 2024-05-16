295a06feb6b451b8d9213f2b11fa928a5b10a619
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-16 17:05:05 -0400

    feat: :sparkles: Verificando se a rede social já esta preenchida, se estiver exibe.

--- views/configuracoes.php
+++ views/configuracoes.php
@@ -1,5 +1,10 @@
 <?php
 require_once 'components/menuLateral.php';
+require_once 'controllers/ConfiguracoesController.php';
+$configuracaoController = new ConfiguracoesController();
+$nomeRedeSocial = 'Whatsapp';
+
+
 ?>
 <div class="main">
     <div class="centralizar">
@@ -9,15 +14,42 @@ require_once 'components/menuLateral.php';
                     <h5>Whatsapp</h5>
                     <form action="salvarRedeSocial" method="post" class="form-container-config">
                         <input type="hidden" name="nomeRede" value="Whatsapp">
-                        <input type="text" name="linkRede">
+                        <?php
+                            $redeSocial = $configuracaoController->verificaSeWhatsappEstaSalvo("Whatsapp");
+                            if ($redeSocial) {
+                            
+                            $valorDoCampoLinkDaTabela = $redeSocial['link'];
+                            ?>
+                            <input type="text" name="linkRede" value="<?php echo $valorDoCampoLinkDaTabela; ?>">
+                        <?php
+                            } else {
+                        ?>
+                            <input type="text" name="linkRede">
+                        <?php
+                            }
+                        ?>
                         <button type="submit">Alterar</button>
                     </form>
+
                 </div>
                 <div class="col-rede-social">
                     <h5>Facebook</h5>
                     <form action="salvarRedeSocial" method="post" class="form-container-config">
                         <input type="hidden" name="nomeRede" value="Facebook">
-                        <input type="text" name="linkRede">
+                        <?php
+                            $redeSocial = $configuracaoController->verificaSeFacebookEstaSalvo("Facebook");
+                            if ($redeSocial) {
+                            
+                            $valorDoCampoLinkDaTabela = $redeSocial['link'];
+                            ?>
+                            <input type="text" name="linkRede" value="<?php echo $valorDoCampoLinkDaTabela; ?>">
+                        <?php
+                            } else {
+                        ?>
+                            <input type="text" name="linkRede">
+                        <?php
+                            }
+                        ?>
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
@@ -25,15 +57,41 @@ require_once 'components/menuLateral.php';
                     <h5>Instagram</h5>
                     <form action="salvarRedeSocial" method="post" class="form-container-config">
                         <input type="hidden" name="nomeRede" value="Instagram">
-                        <input type="text" name="linkRede">
+                        <?php
+                            $redeSocial = $configuracaoController->verificaSeInstagramEstaSalvo("Instagram");
+                            if ($redeSocial) {
+                            
+                            $valorDoCampoLinkDaTabela = $redeSocial['link'];
+                            ?>
+                            <input type="text" name="linkRede" value="<?php echo $valorDoCampoLinkDaTabela; ?>">
+                        <?php
+                            } else {
+                        ?>
+                            <input type="text" name="linkRede">
+                        <?php
+                            }
+                        ?>
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
                 <div class="col-rede-social">
                     <h5>TikTok</h5>
                     <form action="salvarRedeSocial" method="post" class="form-container-config">
-                        <input type="hidden" name="nomeRede" value="TikTok">                        
-                        <input type="text" name="linkRede">
+                        <input type="hidden" name="nomeRede" value="TikTok">
+                        <?php
+                            $redeSocial = $configuracaoController->verificaSeTikTokEstaSalvo("TikTok");
+                            if ($redeSocial) {
+                            
+                            $valorDoCampoLinkDaTabela = $redeSocial['link'];
+                            ?>
+                            <input type="text" name="linkRede" value="<?php echo $valorDoCampoLinkDaTabela; ?>">
+                        <?php
+                            } else {
+                        ?>
+                            <input type="text" name="linkRede">
+                        <?php
+                            }
+                        ?>
                         <button type="submit">Alterar</button>
                     </form>
                 </div>

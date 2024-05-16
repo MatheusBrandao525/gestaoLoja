6f9be8c747c9f1b1facf4e84396b4572962802bc
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-15 21:42:04 -0300

    feat: :sparkles: Página de configurações da aplicação, estilos.

---
+++ views/configuracoes.php
@@ -0,0 +1,71 @@
+<?php
+require_once 'components/menuLateral.php';
+?>
+<div class="main">
+    <div class="centralizar">
+        <div class="config-container">
+            <div class="secao-redes-sociais">
+                <div class="col-rede-social">
+                    <h5>Whatsapp</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>Facebook</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>Instagram</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>TikTok</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="secao-redes-sociais">
+                <div class="col-rede-social">
+                    <h5>Whatsapp</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>Facebook</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>Instagram</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+                <div class="col-rede-social">
+                    <h5>TikTok</h5>
+                    <form action="" method="post" class="form-container-config">
+                        <input type="text">
+                        <button type="submit">Alterar</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>

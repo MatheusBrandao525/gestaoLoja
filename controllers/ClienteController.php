19b86ae1663f84f907b2ea99d63acf6c819d1bda
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-16 10:36:29 -0400

    feat: :sparkles: Buscando últimos clientes cadastrados e quantidade de clientes cadastrados.

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -1,5 +1,7 @@
 <?php
-
+require_once "core/Conexao.php";
+require_once 'utils/Utilidades.php';
+require_once 'models/DAO/ClienteDAO.php';
 class ClienteController
 {
 
@@ -12,4 +14,22 @@ class ClienteController
     {
         include ROOT_PATH . '/views/todosClientes.php';
     }
+
+    public function exibirQuantidadeDeClientesCadastrados()
+    {
+        $conexao = Conexao::getInstance()->getConexao();
+        $clienteDAO = new ClienteDAO($conexao);
+
+        return $quantidadeDeClientesCadastrados = $clienteDAO->contarQuantidadeDeClientesCadastrados();
+    }
+
+    public function exibirUltimosClientesCadastrados()
+    {
+        $conexao = Conexao::getInstance()->getConexao();
+        $clienteDAO = new ClienteDAO($conexao);
+
+        // Chama a função para buscar os últimos clientes cadastrados
+        return $ultimosClientesCadastrados = $clienteDAO->buscarUltimosClientesCadastrados();
+    }
+
 }
---
+++ models/DAO/ClienteDAO.php
@@ -0,0 +1,42 @@
+<?php
+
+class ClienteDAO
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    public function contarQuantidadeDeClientesCadastrados()
+    {
+        $query = "SELECT COUNT(*) AS total FROM clientes";
+
+        try {
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute();
+
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $resultado['total'];
+        } catch (Exception $e) {
+            return ['error' => "Erro ao contar clientes cadastrados: " . $e->getMessage()];
+        }
+    }
+
+    public function buscarUltimosClientesCadastrados()
+    {
+        $query = "SELECT * FROM clientes ORDER BY cliente_id DESC LIMIT 8";
+
+        try {
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute();
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            return ['error' => "Erro ao buscar os últimos clientes cadastrados: " . $e->getMessage()];
+        }
+    }
+
+}

eb79a6d2cf547e45da95abf28108dbac745a2837
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-16 15:15:43 -0400

    refactor: :sparkles: Removendo alert de teste e comentarios.

--- public/assets/js/cadastroProdutoForm.js
+++ public/assets/js/cadastroProdutoForm.js
@@ -1,24 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("formularioCadastroProduto");
   form.addEventListener("submit", function (event) {
-    alert("Evento de submit capturado!");
-    event.preventDefault(); // Impede a submissão padrão do formulário
+    event.preventDefault();
 
-    const formData = new FormData(form); // Coleta os dados do formulário
-    const url = "cadastrarProdutoDatabase"; // URL para onde os dados do formulário serão enviados
+    const formData = new FormData(form);
+    const url = "cadastrarProdutoDatabase";
 
     fetch(url, {
-      method: "POST", // ou 'GET' se necessário
-      body: formData, // Os dados coletados do formulário
+      method: "POST",
+      body: formData,
       headers: {
-        // Especifique cabeçalhos se necessário
       },
     })
-      .then((response) => response.json()) // Converte a resposta do servidor em JSON
+      .then((response) => response.json())
       .then((data) => {
         console.log("Sucesso:", data);
         alert("Produto cadastrado com sucesso!");
-        form.reset(); // Limpa o formulário após o envio
+        form.reset();
       })
       .catch((error) => {
         console.error("Erro:", error);

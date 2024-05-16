03d62b8b17395412eea9782da244d150701ab2ca
MatheusBrandao525 <mafe123silva@gmail.com>
2024-05-16 15:58:58 -0400

    refactor: :recycle: Removendo código html de exemplo.

--- views/configuracoes.php
+++ views/configuracoes.php
@@ -7,60 +7,33 @@ require_once 'components/menuLateral.php';
             <div class="secao-redes-sociais">
                 <div class="col-rede-social">
                     <h5>Whatsapp</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
+                    <form action="salvarRedeSocial" method="post" class="form-container-config">
+                        <input type="hidden" name="nomeRede" value="Whatsapp">
+                        <input type="text" name="linkRede">
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
                 <div class="col-rede-social">
                     <h5>Facebook</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
+                    <form action="salvarRedeSocial" method="post" class="form-container-config">
+                        <input type="hidden" name="nomeRede" value="Facebook">
+                        <input type="text" name="linkRede">
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
                 <div class="col-rede-social">
                     <h5>Instagram</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
+                    <form action="salvarRedeSocial" method="post" class="form-container-config">
+                        <input type="hidden" name="nomeRede" value="Instagram">
+                        <input type="text" name="linkRede">
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
                 <div class="col-rede-social">
                     <h5>TikTok</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
-                        <button type="submit">Alterar</button>
-                    </form>
-                </div>
-            </div>
-
-            <div class="secao-redes-sociais">
-                <div class="col-rede-social">
-                    <h5>Whatsapp</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
-                        <button type="submit">Alterar</button>
-                    </form>
-                </div>
-                <div class="col-rede-social">
-                    <h5>Facebook</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
-                        <button type="submit">Alterar</button>
-                    </form>
-                </div>
-                <div class="col-rede-social">
-                    <h5>Instagram</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
-                        <button type="submit">Alterar</button>
-                    </form>
-                </div>
-                <div class="col-rede-social">
-                    <h5>TikTok</h5>
-                    <form action="" method="post" class="form-container-config">
-                        <input type="text">
+                    <form action="salvarRedeSocial" method="post" class="form-container-config">
+                        <input type="hidden" name="nomeRede" value="TikTok">                        
+                        <input type="text" name="linkRede">
                         <button type="submit">Alterar</button>
                     </form>
                 </div>
